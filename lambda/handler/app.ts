8a8b9477b017bcd800eb54d8af047f123fb758e2
Dion Brown <dion.brown@outlook.co.nz>
2022-03-05 16:46:26 +1300

    Moved gitignore back. Started on lambda.

---
+++ lambda/handler/app.ts
@@ -0,0 +1,38 @@
+import {
+  CloudWatchClient,
+  PutMetricDataCommand,
+  PutMetricDataCommandInput,
+  PutMetricDataCommandOutput,
+} from "@aws-sdk/client-cloudwatch";
+import { Axios } from "axios";
+
+const client = new CloudWatchClient({ region: "REGION" });
+const axios = new Axios();
+export const pingHandler = async () => {
+  axios.get("https://www.google.com").then((res) => {
+    if (res.status != 200) {
+      logCouldwatchError();
+    }
+  });
+};
+
+/**
+ * Sends a failed metric to Cloudwatch metrics
+ */
+async function logCouldwatchError() {
+  const params: PutMetricDataCommandInput = {
+    MetricData: [
+      {
+        Timestamp: new Date(),
+        MetricName: "failed-pings",
+        Value: 1,
+        Unit: "Count",
+        StorageResolution: 1,
+      },
+    ],
+    Namespace: "api-errors",
+  };
+  const command = new PutMetricDataCommand(params);
+  let result: PutMetricDataCommandOutput = await client.send(command);
+  console.log(`Send metric, got result ${result}`);
+}
